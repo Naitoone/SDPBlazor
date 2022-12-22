494166158f97fd7114b6def97cbbf266b3714b2f
Naitoone <dawidwojcieszak6@gmail.com>
2022-12-22 20:54:08 +0100

    Add project files.

---
+++ SDPBlazor/Models/ToDoItemModel.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SDPBlazor.Models
+{
+    public class ToDoItemModel
+    {
+        [StringLength(20, ErrorMessage = "Tittle is too long Max 20 characters")]
+
+        public string Title { get; set; }
+
+        public bool IsDone { get; set; }
+    }
+}
